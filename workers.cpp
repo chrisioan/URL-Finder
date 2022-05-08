aac457c54dcc94281b7c04a5e1d4559663757908
Christos Ioannou <christos_00@hotmail.co.uk>
2022-05-08 15:37:49 +0300

    working on it

--- workers.cpp
+++ workers.cpp
@@ -23,11 +23,11 @@ int main(int argc, char *argv[])
     const string path = argv[0];
     // Initialize Named Pipe name
     const string np_name = NP_DIR + to_string(getpid());
-    int rsize = 0, fd;
-    char read_buf[1000];
+    int rsize = 0, rsize2 = 0, fd_np, fd_read, fd_write;
+    char read_buf[1000], read_buf2[1000];
 
     // Open the Named Pipe for read & write
-    if ((fd = open(np_name.c_str(), O_RDWR)) < 0)
+    if ((fd_np = open(np_name.c_str(), O_RDWR)) < 0)
     {
         // open() failed
         perror("fifo open error");
@@ -38,13 +38,50 @@ int main(int argc, char *argv[])
     {
         // Clear read_buf
         memset(read_buf, 0, 1000);
-        if ((rsize = read(fd, read_buf, 1000)) < 0)
+        if ((rsize = read(fd_np, read_buf, 1000)) < 0)
         {
             // read() failed
             perror("Error in Reading");
             exit(6);
         }
-        printf("Message Received: %.*s\n", rsize, read_buf);
+        string read_file = path;
+        string write_file = "out/";
+        for(int i = 0; i < rsize; i++)
+        {
+            if(read_buf[i] != '\n')
+            {
+                read_file += read_buf[i];
+                write_file += read_buf[i];
+            }
+            else
+                write_file += ".out";
+        }
+        // Open read file
+        if((fd_read = open(read_file.c_str(), O_RDONLY, 0666)) == -1)
+        {
+            // open() failed
+            perror("open call");
+            exit(6);
+
+        }
+        // Create output file
+        if((fd_write = open(write_file.c_str(), O_WRONLY | O_CREAT, 0666)) == -1)
+        {
+            // open() failed
+            if(errno != EEXIST)
+            {
+                // errno not equal to file exists
+                perror("open call");
+                exit(6);
+            }
+        }
+        // Clear read_buf
+        memset(read_buf2, 0, 1000);
+        while((rsize2 = read(fd_read, read_buf2, 1000)) > 0)
+        {
+            printf("%.*s", rsize2, read_buf2);
+        }
+        //printf("Message Received: %.*s\n", rsize, read_buf);
         fflush(stdout);
         raise(SIGSTOP);
     }
